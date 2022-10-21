06fa014d693424a1c28601ee75e95d401730a0ff
Alan Praxedes <alan@opytsuporte.com.br>
2022-10-21 15:24:52 -0300

    commit inicial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import "primereact/resources/themes/tailwind-light/theme.css"
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
